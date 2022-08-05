bbb2f0a9926c99fb7d2cada12ff90021712821ec
Lindiwe Tshabalala <ltshabal021@student.wethinkcode.co.za>
2022-08-05 15:23:58 +0200

    can play video on youtube

--- music-player-automation.py
+++ music-player-automation.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
 
 import os
+import urllib.request
+import re
+import webbrowser
 
 '''
 playsong to run the script
 '''
 
 def choose_song():
-    # song = input("What song would you like to play?")
-    os.system('firefox music.youtube.com')
+    song = input("What video would you like to play?")
+    song = "".join(song.split(" "))
+    url_ = urllib.request.urlopen("https://www.youtube.com/results?search_query="+song)
+    video = re.findall(r"watch\?v=(\S{11})",url_.read().decode())
+
+    webbrowser.open("https://www.youtube.com/watch?v="+video[0])
 
 
 if __name__=='__main__':
